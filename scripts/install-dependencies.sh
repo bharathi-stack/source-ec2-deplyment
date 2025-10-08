cb0468fd87962c7bf5c59f8bc5d5beb9c7a289b6
bharathi-stack <bharathiramachandiran2001@gmail.com>
2025-10-08 12:37:09 +0530

    new

--- scripts/install-dependencies.sh
+++ scripts/install-dependencies.sh
@@ -1,8 +1,6 @@
 # install.sh
 #!/bin/bash
-sudo yum update -y
-if [ -f "requirements.txt" ]; then
-    sudo yum install -r requirements.txt
-fi
-
-
+# sudo yum update -y
+# if [ -f "requirements.txt" ]; then
+#     sudo yum install -r requirements.txt
+# fi
